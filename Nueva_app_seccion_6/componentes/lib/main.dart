1f5b677b25302f9e320b2cb5f3d8b4eb775d50a9
Matias Saffirio <saffiriomatias@gmail.com>
2021-05-25 18:13:56 -0300

    rutas independientes

--- Nueva_app_seccion_6/componentes/lib/main.dart
+++ Nueva_app_seccion_6/componentes/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 // import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
 
@@ -14,10 +15,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
