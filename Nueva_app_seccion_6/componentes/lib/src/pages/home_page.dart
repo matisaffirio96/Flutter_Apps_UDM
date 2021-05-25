cea6493cc2694ae9e9239247279f76bfbff0327a
Matias Saffirio <saffiriomatias@gmail.com>
2021-05-24 23:04:27 -0300

    final video 68

--- Nueva_app_seccion_6/componentes/lib/src/pages/home_page.dart
+++ Nueva_app_seccion_6/componentes/lib/src/pages/home_page.dart
@@ -13,26 +13,41 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _listas() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
+    // print(menuProvider.opciones);
+
+    // menuProvider.cargarData()
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola'),
-      ),
-      Divider(),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.blue,
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.blue,
+        ),
+        onTap: () {},
+      );
+
+      opciones..add(widgetTemp)..add(Divider());
+    });
+
+    return opciones;
   }
 }
